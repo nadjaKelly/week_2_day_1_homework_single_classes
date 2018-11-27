37a406ac106902ea5a67fd9bb953c75e9a977dbe
Nadja Kelly <nmfort-info@yahoo.de>
2018-11-27 23:03:09 +0000

    student cohort can be changed function passed

--- single_class_lab_solutions/student_class/specs/student_specs2.rb
+++ single_class_lab_solutions/student_class/specs/student_specs2.rb
@@ -10,10 +10,29 @@ class StudentTest < MiniTest::Test
 
   end
 
-  # def student_name_test()
-  #   assert_equal("Maya", @student.name)
-  # end
+  # * Create a couple of Getter methods, one that returns the name property and one that returns the cohort property of the student.
+  def test_student_name()
+    assert_equal("Maya", @student.name())
+  end
 
+  def test_student_cohort()
+    assert_equal("E18", @student.cohort())
+  end
+
+# # * Add in Setter methods to update the students name and what cohort they are in.
+#   def test_student_name_update()
+#     @student.name = "Harry"
+#     assert_equal("Harry", @student.name())
+#   end
+#
+#   def test_student_cohort()
+#     @student.cohort = "E10"
+#     assert_equal("E10", @student.cohort())
+#   end
+# # * Create a method that gets the student to talk (eg. Returns "I can talk!).
+#   def test_student_talks()
+#     assert_equal("I can talk", @student.student_talks)
+#   end
 
 
 
--- single_class_lab_solutions/student_class/student2.rb
+++ single_class_lab_solutions/student_class/student2.rb
@@ -2,14 +2,17 @@
 class Student
 
   # * Create a class called Student that takes in a name (String) and a cohort (string - e.g "E18", "G6", etc) when an new instance is created.
+  attr_accessor :name, :cohort
 
  def initialize(name, cohort)
    @name = name
    @cohort = cohort
  end
 
-
-
+# * Create a method that gets the student to talk (eg. Returns "I can talk!).
+  # def student_talks
+  #   return "I can talk"
+  # end
 
 end
 
@@ -20,9 +23,8 @@ end
 
 
 
-# * Create a couple of Getter methods, one that returns the name property and one that returns the cohort property of the student.
-# * Add in Setter methods to update the students name and what cohort they are in.
-# * Create a method that gets the student to talk (eg. Returns "I can talk!).
+
+#
 # * Create a method that takes in a students favourite programming language and returns it as part of a string (eg. `student1.say_favourite_language("Ruby")` -> "I love Ruby").
 #
 #
